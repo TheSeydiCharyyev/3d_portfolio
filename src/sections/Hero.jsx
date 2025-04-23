1994836f3df4080b57b20653ebcd5e4d21cfcb3d
Seydi Charyyev <seydi.charyev@gmail.com>
2025-04-23 11:17:44 +0500

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,7 +10,15 @@ const Hero = () => {
                 {/* LEFT: HERO CONTENT */}
 
                 <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                    <div className='flex flex-col gap-7'>
+                        <div className='hero-text'>
+                            <h1>Shaping</h1>
+                            <h1>into Real projects</h1>
+                            <h1>that Deliver Results</h1>
 
+                        </div>
+
+                    </div>
                 </header>
 
                 {/* RIGHT: 3D Model */}
