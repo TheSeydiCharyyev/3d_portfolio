48105fe7b018b154c80f07701c667815d6e8ee01
Seydi Charyyev <seydi.charyev@gmail.com>
2025-04-23 11:14:45 +0500

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,6 +6,16 @@ const Hero = () => {
             <div className='absolute top-0 left-0 z-10'>
                 <img src='/images/bg.png' alt='background' />
             </div>
+            <div className='hero-layout'>
+                {/* LEFT: HERO CONTENT */}
+
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+
+                </header>
+
+                {/* RIGHT: 3D Model */}
+
+            </div>
         </section>
     )
 }
