7f411fc1faa9e9f622a7a4ee9d6c9bd13f3a123c
Seydi Charyyev <seydi.charyev@gmail.com>
2025-04-24 15:10:30 +0500

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+const words = [
+    { text: 'Ideas', imgPath: '/images/ideas.svg' },
+    { text: 'Concepts', imgPath: '/images/concepts.svg' }
+]
 
 const Hero = () => {
     return (
@@ -12,7 +15,13 @@ const Hero = () => {
                 <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
                     <div className='flex flex-col gap-7'>
                         <div className='hero-text'>
-                            <h1>Shaping</h1>
+                            <h1>Shaping
+                                <span className="slide">
+                                    <span className="wrapper">
+                                        {words.map}
+                                    </span>
+                                </span>
+                            </h1>
                             <h1>into Real projects</h1>
                             <h1>that Deliver Results</h1>
 
