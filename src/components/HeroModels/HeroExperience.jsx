1cb378bd439e0ba811f2efe3a521f76a460280c5
Seydi Charyyev <seydi.charyev@gmail.com>
2025-04-26 16:22:41 +0500

    update

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,9 +1,12 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const HeroExperience = () => {
-    const isTablet = false;
+    const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
     return (
         <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
             <ambientLight intensity={0.2} color="#1a1a40" />
@@ -14,6 +17,10 @@ const HeroExperience = () => {
                 <OrbitControls
                     enablePan={false}
                     enableZoom={!isTablet}
+                    maxDistance={20}
+                    minDistance={5}
+                    minPolarAngle={Math.PI / 5}
+                    maxPolarAngle={Math.PI / 2}
 
                 />
             </mesh>
