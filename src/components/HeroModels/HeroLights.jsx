dbf49c4b64652ba9a6a3bdbdd39dad51b6382fe9
Seydi Charyyev <seydi.charyev@gmail.com>
2025-04-28 16:47:27 +0500

    Update HeroLights.jsx

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -13,8 +13,16 @@ const HeroLights = () => {
                 position={[4, 5, 4]}
                 angle={0.3}
                 intensity={[40]}
-                penumbra={0.2}
-                color="white"
+                penumbra={0.5}
+                color="4cc9f0"
+            />
+
+            <spotLight
+                position={[-3, 5, 5]}
+                angle={0.4}
+                intensity={[60]}
+                penumbra={1}
+                color="9d4edd"
             />
         </>
     )
