cb82b9da4f6ffbd7aa7c604dc12156f78162bd6c
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-05 14:52:32 +0500

    Update ShowcaseSection.jsx

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -8,6 +8,13 @@ const ShowcaseSection = () => {
                     {/* LEFT */}
 
                     <div className='first-project-wrapper'>
+                        <div className='image-wrapper'>
+                            <img src='/images/project1.png' alt='Ryde' />
+                        </div>
+                        <div className='text-content'>
+                            <h2>On-Demand Rides Made Simple with a powerful, User-Friendly App called Ryde</h2>
+
+                        </div>
 
                     </div>
 
