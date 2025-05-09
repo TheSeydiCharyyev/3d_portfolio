73a19c76b885f4ca9ca4229280ac3e21773ffc7d
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-09 20:50:49 +0500

    Update ShowcaseSection.jsx

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 const ShowcaseSection = () => {
     return (
-        <div id='work' className='app-showcase'>
-            <div className='w-full'>
-                <div className='showcaselayout'>
+        <div id='work' className="app-showcase">
+            <div className="w-full">
+                <div className="showcaselayout">
                     {/* LEFT */}
 
-                    <div className='first-project-wrapper'>
-                        <div className='image-wrapper'>
+                    <div className="first-project-wrapper">
+                        <div className="image-wrapper">
                             <img src='/images/project1.png' alt='Ryde'
                             />
-                            <div className='text-content'>
+                            <div className="text-content">
                                 <h2>On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde</h2>
+                                <p className="text-white-50 md:text-xl">
+                                    An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+
+                                </p>
 
                             </div>
 
