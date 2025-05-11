5a67c7eff47a6b05d13b83f746041a70d5cdd466
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-11 16:41:17 +0500

    Update ShowcaseSection.jsx

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -14,6 +14,25 @@ const ShowcaseSection = () => {
 
     const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
 
+    cardsforEach((card, index) => {
+        gsap.fromTo(
+            card,
+            {
+                y: 50, opacity: 0
+            },
+            {
+                y: 0,
+                opacity: 1,
+                duration: 1,
+                delay: 0.3 * (index + 1),
+                ScrollTrigger: {
+                    trigger: card,
+                }
+            }
+
+        )
+    })
+
 
     useGSAP(() => {
         gsap.fromTo(sectionRef.current,
