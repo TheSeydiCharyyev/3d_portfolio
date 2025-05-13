7b9971b579a479db274af5ffaf006a50a43e9dd3
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-13 16:29:33 +0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { navLinks } from '../constants'
 
 const NavBar = () => {
     const [scrolled, setScrolled] = useState(false);
+
     useEffect(() => {
         const handleScroll = () => {
             const isScrolled = window.scrollY > 10;
@@ -10,36 +11,39 @@ const NavBar = () => {
         }
 
         window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
     }, [])
 
 
     return (
-        <header className='navbar'>
+        <header className={`navbar ${scrolled ? 'scrolled:
+            'not-scrolled'
+}` }>
+    < div className='inner'>
+        <a className='logo' href='#hero'>
+            Seydi Charyyev
+        </a>
+        <nav className='desktop'>
+            <ul>
+                {navLinks.map(({ link, name }) => (
+                    <li key={name} className='group'>
+                        <a href={link}>
+                            <span>{name}</span>
+                            <span className='underline' />
+
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+        <a href='#contact' className='contact-btn group'>
             <div className='inner'>
-                <a className='logo' href='#hero'>
-                    Seydi Charyyev
-                </a>
-                <nav className='desktop'>
-                    <ul>
-                        {navLinks.map(({ link, name }) => (
-                            <li key={name} className='group'>
-                                <a href={link}>
-                                    <span>{name}</span>
-                                    <span className='underline' />
+                <span>Contact me</span>
 
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <a href='#contact' className='contact-btn group'>
-                    <div className='inner'>
-                        <span>Contact me</span>
-
-                    </div>
-
-                </a>
             </div>
+
+        </a>
+    </div>
 
         </header >
     )
