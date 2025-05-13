4fc75f8898bf39cc2f338600b0fc628784b22aa4
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-13 15:56:26 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
 import Hero from "./sections/hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
 const app = () => {
     return (
         <>
+            <NavBar />
             <Hero />
             <ShowcaseSection />
         </>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header className='navbar'>
+
+        </header>
+    )
+}
+
+export default NavBar
