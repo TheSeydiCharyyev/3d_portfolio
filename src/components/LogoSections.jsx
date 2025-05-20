fda655bba17ce7ff9c92e70983f322e904ee585f
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-20 15:10:39 +0500

    Update LogoSections.jsx

--- src/components/LogoSections.jsx
+++ src/components/LogoSections.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const LogoSections = () => {
     return (
-        <div>LogoSections</div>
+        <div className='md: my-20 my-20 relative'>
+            <div className='gradient-edge' />
+            <div className='gradient-edge' />
+
+            <div className='marquee h-52'>
+                <div className='marquee-box md:gap-12 gap-5'>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 
