b0bcc15eb8b895f1e9caa63ad443763b374fac64
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-21 11:12:06 +0500

    Update FeatureCards.jsx

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -6,6 +6,9 @@ const FeatureCards = () => {
             <div className='mx-auto grid-3-col'>
                 {abilities.map(({ imgPath, title, desc }) => (
                     <div key={title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
+                        <div className='size-14 flex items-center justify-center rounded-full'>
+                            <img src={imgPath} alt={title} />
+                        </div>
                         <h3>
                             {title}
                         </h3>
