4e70de74015e052300b62913566a8f57c3e6116f
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-22 17:04:19 +0500

    Sub class

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -5,7 +5,8 @@ const ExperienceSection = () => {
     return (
         <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl:px-0">
             <div className='w-full h-full md:px-20 px-5'>
-                <TitleHeader title='Professional Work Experience' sub="My Career Overview" />
+                <TitleHeader title='Professional Work Experience'
+                    sub="My Career Overview" />
             </div>
         </section>
     )
