89a113cd404accbc947cfd33e4373832059c3556
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-22 16:48:09 +0500

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ExperienceSection = () => {
     return (
-        <div>ExperienceSection</div>
+        <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl:px-0">ExperienceSection</section>
     )
 }
 
