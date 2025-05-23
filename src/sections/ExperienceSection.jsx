373f4f7b1cf9693947fdc6c4e07661b86af36b73
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-23 15:00:36 +0500

    GlowCard

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,3 +1,4 @@
+import GlowCard from '../components/GlowCard'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants/index'
 
@@ -11,8 +12,12 @@ const ExperienceSection = () => {
                     <div className='relative z-50 xl:space-y-32 space-y-10'>
                         {expCards.map((card) => (
                             <div key={card.title} className='exp-card-wrapper'>
-                                {card.title}
+                                <div className='xl:w-2/6'>
+                                    <GlowCard>
 
+                                    </GlowCard>
+
+                                </div>
                             </div>
                         ))}
 
