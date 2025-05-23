d87dcdeff8c0f024cdd6e1ceadcdf409b00da5fb
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-23 14:57:31 +0500

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -10,7 +10,8 @@ const ExperienceSection = () => {
                 <div className='mt-32 relative'>
                     <div className='relative z-50 xl:space-y-32 space-y-10'>
                         {expCards.map((card) => (
-                            <div key={card.title}>
+                            <div key={card.title} className='exp-card-wrapper'>
+                                {card.title}
 
                             </div>
                         ))}
