89969b3561c26d9437504ad89b30efd44051fb2a
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-23 15:11:06 +0500

    Update GlowCard.jsx

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -4,7 +4,12 @@ const GlowCard = ({ card, children }) => {
     return (
         <div className='card car-border time-card rounded-xl p-10'>
             <div>
-                <div />
+                <div className='glow' />
+                <div>
+                    {Array.from({ length: 5 }, (_, i) => (
+                        <img src='' />
+                    ))}
+                </div>
             </div>
 
         </div>
