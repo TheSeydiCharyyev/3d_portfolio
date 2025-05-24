dee5abb22da0398afb68c10bc7b4a83f2441043c
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-24 16:16:48 +0500

    Update GlowCard.jsx

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -12,6 +12,10 @@ const GlowCard = ({ card, children, index }) => {
         const rect = card.getBoundingClientRect();
         const mouseX = e.clientX - rect.left - rect.width / 2;
         const mouseY = e.clientY - rect.top - rect.height / 2;
+
+        // calc the angle from the center of the card
+
+        let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI)
     }
 
     return (
